ce6e9bbcb74deab52a7405d13fa9b996d5f56e4a
AvielNiego <avielniego@gmail.com>
2016-03-08 22:11:14 +0200

    Add the get players details stub functions to View

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,22 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class View
 {
+    public int getHumanPlayersNumber()
+    {
+        return 0;
+    }
+
+    public int getComputerPlayersNumber()
+    {
+        return 0;
+    }
+
+    public List<String> getHumanPlayerNames(int humanPlayersNumber)
+    {
+        return new ArrayList<>();
+    }
 }
