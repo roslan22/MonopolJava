5bab53b9863e0117388ee6db4a1b3d39e598bf2d
AvielNiego <avielniego@gmail.com>
2016-03-11 20:07:21 +0200

    Reformat

--- src/com/monopoly/logic/model/cell/City.java
+++ src/com/monopoly/logic/model/cell/City.java
@@ -5,12 +5,12 @@ import com.monopoly.logic.model.player.Player;
 public class City extends Property
 {
     private static final int MAX_HOUSES_AVAILABLE = 3;
-    public static final int RENT_PRICES_AMOUNT = MAX_HOUSES_AVAILABLE + 1;
+    public static final  int RENT_PRICES_AMOUNT   = MAX_HOUSES_AVAILABLE + 1;
 
     private String cityName;
     private int houseCounter = 0;
-    private int housePrice;
-    private int[] rentPrices;
+    private int     housePrice;
+    private int[]   rentPrices;
     private Country country;
 
     public City(String cityName, int propertyPrice, int housePrice, int[] rentPrices)
@@ -29,7 +29,9 @@ public class City extends Property
     private void setRentPrices(int[] rentPrices)
     {
         if (rentPrices.length != RENT_PRICES_AMOUNT)
+        {
             throw new IllegalRentPricesAmount();
+        }
         this.rentPrices = rentPrices;
     }
 
@@ -39,9 +41,9 @@ public class City extends Property
     }
 
     @Override
-    public void perform(Player player) 
+    public void perform(Player player)
     {
-        if (canBuyHouse(player)) 
+        if (canBuyHouse(player))
         {
             buyHouse(player);
         }
@@ -51,24 +53,25 @@ public class City extends Property
         }
     }
 
-    private boolean canBuyHouse(Player player) 
+    private boolean canBuyHouse(Player player)
     {
         return getOwner().equals(player) && country.hasMonopoly(player);
     }
 
-    private void buyHouse(Player player) {
+    private void buyHouse(Player player)
+    {
         if (player.getMoney() >= housePrice && houseCounter < MAX_HOUSES_AVAILABLE)
         {
             if (offerToBuyHouse(player))
             {
-                
+
                 houseCounter++;
             }
         }
     }
 
     @Override
-    public int getRentPrice() 
+    public int getRentPrice()
     {
         return rentPrices[houseCounter];
     }
