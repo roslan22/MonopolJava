c7d50742bc8762c8a609d99118089324454b1703
AvielNiego <avielniego@gmail.com>
2016-03-11 19:54:57 +0200

    Add hasMonopoly method

--- src/com/monopoly/logic/model/cell/Country.java
+++ src/com/monopoly/logic/model/cell/Country.java
@@ -1,5 +1,25 @@
 package com.monopoly.logic.model.cell;
 
+import com.monopoly.logic.model.player.Player;
+
+import java.util.List;
+
 public class Country
 {
+    private List<City> countryCities;
+
+    public Country(List<City> countryCities)
+    {
+        this.countryCities = countryCities;
+    }
+
+    public boolean hasMonopoly(Player player)
+    {
+        for (City city : countryCities)
+        {
+            if (!city.getOwner().equals(player))
+                return false;
+        }
+        return true;
+    }
 }
