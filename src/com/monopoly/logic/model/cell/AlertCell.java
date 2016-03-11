4f13f512371bbe0cada923b6ff83eb5b34edaab4
AvielNiego <avielniego@gmail.com>
2016-03-11 19:53:25 +0200

    Add CardPack member

--- src/com/monopoly/logic/model/cell/AlertCell.java
+++ src/com/monopoly/logic/model/cell/AlertCell.java
@@ -1,13 +1,21 @@
 package com.monopoly.logic.model.cell;
 
+import com.monopoly.logic.model.card.AlertCard;
 import com.monopoly.logic.model.card.Card;
+import com.monopoly.logic.model.card.CardPack;
 
 public class AlertCell extends CardCell
 {
+    private CardPack<AlertCard> alertCardPack;
+
+    public AlertCell(CardPack<AlertCard> alertCardPack)
+    {
+        this.alertCardPack = alertCardPack;
+    }
 
     @Override
     public Card getCard() 
     {
-        return getAlertCardPack().getNext();
+        return alertCardPack.getNext();
     }
 }
--- src/com/monopoly/logic/model/cell/SurpriseCell.java
+++ src/com/monopoly/logic/model/cell/SurpriseCell.java
@@ -1,12 +1,21 @@
 package com.monopoly.logic.model.cell;
 
 import com.monopoly.logic.model.card.Card;
+import com.monopoly.logic.model.card.CardPack;
+import com.monopoly.logic.model.card.SurpriseCard;
 
 public class SurpriseCell extends CardCell
 {
+    private CardPack<SurpriseCard> surpriseCardPack;
+
+    public SurpriseCell(CardPack<SurpriseCard> surpriseCardPack)
+    {
+        this.surpriseCardPack = surpriseCardPack;
+    }
+
     @Override
     public Card getCard() 
     {
-        return getSurpriseCardPack().getNext();
+        return surpriseCardPack.getNext();
     }
 }
