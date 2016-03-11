bcd87c5351441fb3103e53161c254c18b47145ea
AvielNiego <avielniego@gmail.com>
2016-03-11 19:59:05 +0200

    Add Property ctor and City's getRentPrice

--- src/com/monopoly/logic/model/cell/City.java
+++ src/com/monopoly/logic/model/cell/City.java
@@ -5,9 +5,32 @@ import com.monopoly.logic.model.player.Player;
 public class City extends Property
 {
     private static final int MAX_HOUSES_AVAILABLE = 3;
-    
+    public static final int RENT_PRICES_AMOUNT = MAX_HOUSES_AVAILABLE + 1;
+
+    private String cityName;
     private int houseCounter = 0;
     private int housePrice;
+    private int[] rentPrices;
+
+    public City(String cityName, int propertyPrice, int housePrice, int[] rentPrices)
+    {
+        super(propertyPrice);
+        this.cityName = cityName;
+        this.housePrice = housePrice;
+        setRentPrices(rentPrices);
+    }
+
+    private void setRentPrices(int[] rentPrices)
+    {
+        if (rentPrices.length != RENT_PRICES_AMOUNT)
+            throw new IllegalRentPricesAmount();
+        this.rentPrices = rentPrices;
+    }
+
+    public String getCityName()
+    {
+        return cityName;
+    }
 
     @Override
     public void perform(Player player) 
@@ -41,6 +64,10 @@ public class City extends Property
     @Override
     public int getRentPrice() 
     {
-        
+        return rentPrices[houseCounter];
+    }
+
+    public static class IllegalRentPricesAmount extends RuntimeException
+    {
     }
 }
--- src/com/monopoly/logic/model/cell/Property.java
+++ src/com/monopoly/logic/model/cell/Property.java
@@ -6,34 +6,34 @@ public abstract class Property extends Cell
 {
     private Player owner;
     private int price;
-    
+
+    public Property(int price)
+    {
+        this.price = price;
+    }
+
+    public Player getOwner()
+    {
+        return owner;
+    }
+
+    public void setOwner(Player owner)
+    {
+        this.owner = owner;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
     public abstract int getRentPrice();
-    
+
     public boolean isPropertyAvailable()
     {
         return owner == null;
     }
 
-    public Player getOwner() 
-    {
-        return owner;
-    }
-
-    public void setOwner(Player owner) 
-    {
-        this.owner = owner;
-    }
-
-    public int getPrice() 
-    {
-        return price;
-    }
-
-    public void setPrice(int price) 
-    {
-        this.price = price;
-    }
-    
     @Override
     public void perform(Player player) {
         if (isPropertyAvailable()) 
