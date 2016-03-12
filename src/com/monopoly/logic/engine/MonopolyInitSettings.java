13b2152f978ac41756b923c51b4323629059244e
AvielNiego <avielniego@gmail.com>
2016-03-12 16:50:44 +0200

    Add getCells method

--- src/com/monopoly/logic/engine/MonopolyInitSettings.java
+++ src/com/monopoly/logic/engine/MonopolyInitSettings.java
@@ -2,7 +2,14 @@
 package com.monopoly.logic.engine;
 
 
+import com.monopoly.logic.model.cell.Cell;
+
+import java.util.List;
+
 public class MonopolyInitSettings
 {
-    
+    public List<Cell> getCells()
+    {
+        return new DoNotCompile();
+    }
 }
