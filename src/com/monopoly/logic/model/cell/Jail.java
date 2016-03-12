42b8a472eb88d684e1db9ec4785aa5e1af058a3c
AvielNiego <avielniego@gmail.com>
2016-03-12 12:06:54 +0200

    Jail's perform method does nothing (free pass).
    Should implement the jail

--- src/com/monopoly/logic/model/cell/Jail.java
+++ src/com/monopoly/logic/model/cell/Jail.java
@@ -1,5 +1,9 @@
 package com.monopoly.logic.model.cell;
 
+import com.monopoly.logic.model.player.Player;
+
 public class Jail extends Cell
 {
+    @Override
+    public void perform(Player player) {}
 }
