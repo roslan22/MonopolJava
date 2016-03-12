e6480bc680dc474c84bb0e8851ca94bf6af56dd8
AvielNiego <avielniego@gmail.com>
2016-03-12 11:59:33 +0200

    Add equals method

--- src/com/monopoly/logic/model/cell/Cell.java
+++ src/com/monopoly/logic/model/cell/Cell.java
@@ -6,4 +6,10 @@ import com.monopoly.logic.model.player.Player;
 public abstract class Cell 
 {
     abstract public void perform(Player player);
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return this == obj;
+    }
 }
