8ae6121018f6a13ac8a70527bb1c7131c9df9c26
AvielNiego <avielniego@gmail.com>
2016-03-12 12:05:25 +0200

    Implement Company

--- src/com/monopoly/logic/model/cell/Company.java
+++ src/com/monopoly/logic/model/cell/Company.java
@@ -2,5 +2,25 @@ package com.monopoly.logic.model.cell;
 
 public class Company extends Property
 {
-    
+    private String    name;
+    private final int rentPrice;
+    private final int monopolyRentPrice;
+
+    public Company(String name, int propertyPrice, int rentPrice, int monopolyRentPrice)
+    {
+        super(propertyPrice);
+        this.name = name;
+        this.rentPrice = rentPrice;
+        this.monopolyRentPrice = monopolyRentPrice;
+    }
+
+    @Override
+    public int getRentPrice()
+    {
+        if(getPropertyGroup().hasMonopoly(getOwner()))
+        {
+            return monopolyRentPrice;
+        }
+        return rentPrice;
+    }
 }
