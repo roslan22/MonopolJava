8db4379a8a65b96e55ad403b49fe6ed1da757812
roslan22 <roslan22@gmail.com>
2016-03-12 22:23:45 +0200

    implementation of View class

--- src/com/monopoly/view/View.java
+++ src/com/monopoly/view/View.java
@@ -2,21 +2,49 @@ package com.monopoly.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class View
 {
+    private Scanner scanner = new Scanner(System.in);
+
     public int getHumanPlayersNumber()
     {
-        return 0;
+        System.out.print("Please enter a number of Human players: ");
+	int humanPlayers  = scanner.nextInt();
+        
+        return humanPlayers;
     }
 
     public int getComputerPlayersNumber()
     {
-        return 0;
+        System.out.print("Please enter a number of Computer players: ");
+	int humanPlayers  = scanner.nextInt();
+        
+        return humanPlayers;
     }
 
     public List<String> getHumanPlayerNames(int humanPlayersNumber)
     {
-        return new ArrayList<>();
+        List<String> playerNames = new ArrayList<String>();
+        String inputName;
+        
+        System.out.println("Please enter names of human players: ");
+
+        for(int i = 0; i < humanPlayersNumber; i++)
+        {
+            System.out.println("Player number " + i + " is:");
+            inputName = scanner.nextLine();  //TODO: check if it's ok
+            playerNames.add(inputName);
+            //clear the scanner buffer from the Enter
+            scanner.nextLine();
+        }
+        
+        return playerNames;
+    }
+    
+    public void BoardChange(String change)
+    {
+        //TODO: implement
     }
 }
