de1f9569e424fc23a5ba31488548b9da9650807f
AvielNiego <avielniego@gmail.com>
2016-04-08 18:41:32 +0300

    Another refactors

--- src/com/monopoly/main/Main.java
+++ src/com/monopoly/main/Main.java
@@ -9,6 +9,24 @@ public class Main
 {
     public static void main(String[] args)
     {
+        playMonopoly();
+    }
+
+    private static void playMonopoly()
+    {
+        try
+        {
+            startController();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            playMonopoly();
+        }
+    }
+
+    private static void startController()
+    {
         Controller controller = new Controller(new View(), new MonopolyEngine());
         controller.play();
     }
